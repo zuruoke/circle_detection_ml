9bcbd746f92710bcce5cbfb3a1695ed7da39632e
zuruoke <zuruokeokafor2016@gmail.com>
2023-12-01 03:46:18 +0000

    reformat parser

--- args.py
+++ args.py
@@ -11,9 +11,9 @@ def args_parser():
     parser.add_argument("--test_dataset_size", type=int, default=100, help="Size of the testing dataset")
     parser.add_argument("--img_shape", type=int, default=64, help="Image shape")
     parser.add_argument("--noise_level", type=float, default=0.5, help="Noise level")
-    parser.add_argument("--loss", type=str, default='mse', help="Loss function")
-    parser.add_argument("--optimizer", type=str, default='Adam', help="Optimizer")
-    parser.add_argument("--model", type=str, default='resnet', help="Model architecture")
+    parser.add_argument("--loss", type=str, default='mse', choices=["mse", "mae", "cross_entropy"], help="Loss function")
+    parser.add_argument("--optimizer", type=str, default='Adam', choices=["Adam", "SGD", "RMSProp"], help="Optimizer")
+    parser.add_argument("--model", type=str, default='resnet', choices=["resnet", "unet"], help="Model architecture")
     parser.add_argument("--dropout", type=float, default=0.5, help="Dropout rate")
 
     args = parser.parse_args()
---
+++ main.py
@@ -0,0 +1,17 @@
+from args import args_parser
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    args = args_parser()
+    print(args)
