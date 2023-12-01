29564c007c3acd42695c330016a4d570ec0b9ae9
zuruoke <zuruokeokafor2016@gmail.com>
2023-12-01 21:34:49 +0000

    hot fixes

--- test.py
+++ test.py
@@ -7,34 +7,6 @@ from dataset.dataset_utils import CircleParams
 from helpers.calculate_iou import iou
 
 
-# def predict(model, test_loader, criterion):
-#     model.eval()  # Set the model to evaluation mode
-#     predictions = []
-#     total_loss = 0.0
-#     with torch.no_grad():  # No need to track gradients
-#         for images, labels in test_loader:
-#             outputs = model(images)
-#             loss = criterion(outputs, labels)
-#             predicted_params = outputs.numpy()
-#             predictions.extend(predicted_params)
-#             total_loss += loss.item()
-#     return predictions, total_loss
-
-
-
-# def evaluate(model, test_loader, criterion):
-#     predictions, loss = predict(model, test_loader, criterion)
-#     ious = []
-#     for i, (_, true_params) in enumerate(test_loader):
-#         predicted_params = CircleParams(*predictions[i])
-#         # Ensure true_params are correctly unpacked
-#         true_params = CircleParams(*true_params.numpy().squeeze())
-#         iou_score = iou(predicted_params, true_params)
-#         ious.append(iou_score)
-#     return np.mean(ious), loss/len(test_loader)
-
-
-
 def predict(model: nn.Module,
             test_loader: DataLoader,
             criterion: Type[nn.Module]) -> Tuple[List[np.ndarray], float]:
--- train.py
+++ train.py
@@ -6,32 +6,6 @@ from tqdm import tqdm
 from test import evaluate_model
 from dataset.dataset_loader import CircleDataset
 
-
-
-# def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs=100):
-#     for epoch in range(num_epochs):
-#         # Training phase
-#         model.train()
-#         running_train_loss = 0.0
-
-#         # Use tqdm for the progress bar
-#         train_loader_with_progress = tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}, Training")
-
-#         for images, labels in train_loader_with_progress:
-#             optimizer.zero_grad()
-#             outputs = model(images)
-#             loss = criterion(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
-#             running_train_loss += loss.item()
-#             train_loader_with_progress.set_postfix({"Training Loss": running_train_loss/len(train_loader)})
-
-#         # Validation phase
-#         model.eval()
-#         ious, val_loss = evaluate(model, val_loader, criterion)
-#         print(f"Epoch [{epoch+1}/{num_epochs}], Validation Loss: {val_loss}")
-#         print(f"Epoch [{epoch+1}/{num_epochs}], Average Validation IoU: {ious}")
-
 def train_model(model: nn.Module,
                 train_loader: DataLoader,
                 val_loader: DataLoader,
