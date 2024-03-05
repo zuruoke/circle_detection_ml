7d1dd3901cffe3b023d0dd7094d76e66782adc1a
Zuruoke Okafor <51057490+zuruoke@users.noreply.github.com>
2024-03-05 23:27:30 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,28 +1,25 @@
-"""Forms for playlist app."""
-
-from wtforms import SelectField, TextAreaField, StringField
-from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired, Length
-
-
-class PlaylistForm(FlaskForm):
-    """Form for adding playlists."""
-
-    # Add the necessary code to use this form
-    name = StringField('Playlist Name', validators=[InputRequired(), Length(min=2, max=30)])
-    description = TextAreaField('Playlist Description', validators=[Length(max=150)])
-
-
-class SongForm(FlaskForm):
-    """Form for adding songs."""
-
-    # Add the necessary code to use this form
-    title = StringField('Song Title', validators=[InputRequired(), Length(min=2, max=100)])
-    artist = StringField('Artist', validators=[InputRequired(), Length(min=2, max=100)])
-
-
-# DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
-class NewSongForPlaylistForm(FlaskForm):
-    """Form for adding a song to playlist."""
-
-    song = SelectField('Song To Add', coerce=int)
+{% extends "base.html" %} {% block content %}
+<h1>Add Playlist</h1>
+{% if get_flashed_messages() %} {% for message in
+get_flashed_messages(category_filter=["error"]) %}
+<div class="alert alert-danger">{{ message }}</div>
+{% endfor %} {% endif %}
+<form method="POST" action="{{ url_for('add_playlist') }}">
+  {{ form.hidden_tag() }}
+  <div class="form-group">
+    {{ form.name.label(class="form-control-label") }} {{
+    form.name(class="form-control") }} {% if form.name.errors %}
+    <div class="alert alert-danger" role="alert">{{ form.name.errors[0] }}</div>
+    {% endif %}
+  </div>
+  <div class="form-group">
+    {{ form.description.label(class="form-control-label") }} {{
+    form.description(class="form-control") }} {% if form.description.errors %}
+    <div class="alert alert-danger" role="alert">
+      {{ form.description.errors[0] }}
+    </div>
+    {% endif %}
+  </div>
+  <button type="submit" class="btn btn-primary">Create</button>
+</form>
+{% endblock %}
