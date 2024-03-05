3a0f99770c582a30068889be2c359ab0e111868a
Zuruoke Okafor <51057490+zuruoke@users.noreply.github.com>
2024-03-05 23:29:13 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,14 +1,23 @@
 {% extends "base.html" %} {% block content %}
-<h2>Add Song to "{{ playlist.name }}"</h2>
-<form method="POST">
-  {{ form.hidden_tag() }}
-  <div class="form-group">
-    {{ form.song.label(class="form-control-label") }} {{
-    form.song(class="form-control") }} {% if form.song.errors %} {% for error in
-    form.song.errors %}
-    <div class="alert alert-danger" role="alert">{{ error }}</div>
-    {% endfor %} {% endif %}
-  </div>
-  <button type="submit" class="btn btn-primary">Add Song</button>
-</form>
+<div class="container mt-5">
+  <h2>Add a New Song</h2>
+  <form method="POST" action="">
+    {{ form.hidden_tag() }}
+    <div class="form-group">
+      {{ form.title.label(class="form-control-label") }} {{
+      form.title(class="form-control") }} {% if form.title.errors %} {% for
+      error in form.title.errors %}
+      <div class="alert alert-danger" role="alert">{{ error }}</div>
+      {% endfor %} {% endif %}
+    </div>
+    <div class="form-group">
+      {{ form.artist.label(class="form-control-label") }} {{
+      form.artist(class="form-control") }} {% if form.artist.errors %} {% for
+      error in form.artist.errors %}
+      <div class="alert alert-danger" role="alert">{{ error }}</div>
+      {% endfor %} {% endif %}
+    </div>
+    <button type="submit" class="btn btn-primary">Add Song</button>
+  </form>
+</div>
 {% endblock %}
